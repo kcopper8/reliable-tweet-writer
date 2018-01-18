8bcdd8989d3dfb6dcb88a917389dfeede70f5aa4
kcopper8 <kcopper8@gmail.com>
2018-01-19 01:20:49 +0900

    vue-cli 기반 프로젝트 구성

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="wrap">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html,body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+#wrap {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0 10px;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,98 @@
+<template>
+<div class="wrap">
+  <div class="box">
+    <textarea class="text" v-model="message" placeholder="tweet contents"></textarea>
+  </div>
+  <div class="function_wrap">
+    <a class="button submitText" :href="tweetIntentUrl" target="TweetWindow">tweet</a>
+    <button class="button" @click="clear">clear</button>
+  </div>
+</div>
+</template>
+
+<script>
+const DataService = {
+  save(message) {
+    localStorage.lastText = message;
+  },
+  clear() {
+    localStorage.lastText = '';
+  },
+  load() {
+    if (localStorage.lastText) {
+      return localStorage.lastText;
+    }
+
+    return '';
+  },
+};
+
+export default {
+  name: 'Editor',
+
+  data() {
+    return {
+      message: DataService.load(),
+    };
+  },
+
+  computed: {
+    tweetIntentUrl() {
+      return `https://twitter.com/intent/tweet?text=${encodeURIComponent(this.message)}`;
+    },
+  },
+
+  watch: {
+    message(message) {
+      DataService.save(message);
+    },
+  },
+
+  methods: {
+    clear() {
+      DataService.clear();
+      this.message = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrap {
+  width: 100%;
+  height: 100%;
+}
+
+.box {
+  padding: 10px 0 150px;
+  height: 100%;
+}
+
+.text {
+  width: 100%;
+  height: 100%;
+}
+
+.function_wrap {
+  position: fixed;
+  bottom: 105px;
+}
+
+.submitText {
+  border: solid 1px;
+  border-width: 2px;
+  border-style: outset;
+  -webkit-appearance: button;
+  text-transform: none;
+  text-indent: 0px;
+  font: 400 13.3px Arial;
+  text-align: center;
+  color: buttontext;
+  text-decoration: none;
+}
+
+.button {
+  padding: 10px;
+}
+
+</style>
