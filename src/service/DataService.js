bab1134a782a389bfc5b0418be03a5deccd20885
kcopper8 <kcopper8@gmail.com>
2018-03-01 03:44:17 +0900

    안쓰는 코드 제거

--- src/service/DataService.js
+++ src/service/DataService.js
@@ -6,10 +6,6 @@ function extractIdFromThreadKey(key) {
   return parseInt(key.substr('thread.'.length), 10);
 }
 
-function threadKey(id) {
-  return `thread.${id}`;
-}
-
 function replyIdKey(id) {
   return `replyId.${id}`;
 }
@@ -36,10 +32,6 @@ export default class DataService {
       this._currentThreadId = 0;
     }
     return parseInt(this.storage.currentThreadId, 10);
-  }
-
-  get _currentThreadKey() {
-    return threadKey(this._currentThreadId);
   }
 
   migrate() {
@@ -85,53 +77,6 @@ export default class DataService {
     this.storage['rtw.version'] = '1.2';
   }
 
-  save(message) {
-    this.storage[this._currentThreadKey] = message;
-  }
-
-  saveReplyId(replyId) {
-    this.storage[replyIdKey(this._currentThreadId)] = replyId;
-  }
-
-  loadReplyId() {
-    return this.storage[replyIdKey(this._currentThreadId)];
-  }
-
-  removeReplyId() {
-    delete this.storage[replyIdKey(this._currentThreadId)];
-  }
-
-  clear(threadId) {
-    delete this.storage[threadKey(threadId)];
-    delete this.storage[replyIdKey(threadId)];
-  }
-
-  addNewThread(thread) {
-    this._currentThreadId = thread.id;
-  }
-
-  newThread() {
-    const currentMaxThreadId = Object.entries(this.storage)
-      .reduce((lastMaxThreadId, entry) => {
-        if (isThreadKey(entry[0])) {
-          const id = extractIdFromThreadKey(entry[0]);
-          if (id > lastMaxThreadId) {
-            return id;
-          }
-        }
-
-        return lastMaxThreadId;
-      }, this._currentThreadId);
-
-    this._currentThreadId = currentMaxThreadId + 1;
-    this.save('');
-  }
-
-  load() {
-    const lastText = this.storage[`thread.${this._currentThreadId}`];
-    return lastText || '';
-  }
-
   _threadList11() {
     return Object.entries(this.storage)
       .filter(entry => isThreadKey(entry[0]))
@@ -149,15 +94,10 @@ export default class DataService {
     return this._data.threads;
   }
 
-  setCurrentThread(threadId) {
-    this._currentThreadId = threadId;
-  }
-
   /**
    * 저장된 값이 없으면 0 이 반환된다.
    */
   getCurrentThreadId() {
-    // return this._currentThreadId;
     return this._data.editingIndex;
   }
 }
